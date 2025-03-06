e963483c635bb2f8dae755dabc77c5e318bcc1ad
Darwin Gomez <darwingomez@wposs.com>
2025-03-06 15:11:16 -0500

    SCRUM-21 Se implementa funcionalidad con el fin de iniciar el servidor de angular al momento de iniciar el backEnd

--- back_end/src/main/java/com/example/demo/DemoApplication.java
+++ back_end/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,31 @@
 package com.example.demo;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		startAngular();
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-		System.out.println("hola mundo");
+	private static void startAngular() {
+		try {
+			String backendPath = System.getProperty("user.dir");
+			File frontendDir = new File(backendPath, "../front_end");
+			String frontendPath = frontendDir.getAbsoluteFile().getCanonicalPath() + File.separator + "front_end_tickets_bus_pdn";
+
+			ProcessBuilder processBuilder = new ProcessBuilder("cmd.exe", "/c", "start ng serve --open");
+			processBuilder.directory(new File(frontendPath));
+
+			processBuilder.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
