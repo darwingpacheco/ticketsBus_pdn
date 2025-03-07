be19d930e9962bb5d4da368be27b27cf5bc46227
Darwin Gomez <darwingomez@wposs.com>
2025-03-07 07:37:55 -0500

    SCRUM-19 Se implementa FIREBASE para la autenticación.

--- front_end/front_end_tickets_bus_pdn/src/app/app.routes.ts
+++ front_end/front_end_tickets_bus_pdn/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponentComponent } from './components/login/login-component/login-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponentComponent,
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'login',
+      }
+];
