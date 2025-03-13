152133529105d1743945d7cd8d578dee6ffb3379
Darwin Gomez <darwingomez@wposs.com>
2025-03-13 14:02:56 -0500

    SCRUM-30 Se implementan los validator para cada campo del login y crear cuenta de usuario

--- front_end/front_end_tickets_bus_pdn/src/app/components/createAccount/create-account-components/create-account-components.component.ts
+++ front_end/front_end_tickets_bus_pdn/src/app/components/createAccount/create-account-components/create-account-components.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../../services/auth-service.service';
 
 @Component({
   selector: 'app-create-account-components',
   standalone: true,
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule ],
   templateUrl: './create-account-components.component.html',
   styleUrl: './create-account-components.component.css'
 })
 export class CreateAccountComponentsComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService: AuthService) {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      phone: ['', Validators.required],
+    });
+  }
+
+  register() {
+    console.log("presionaste agregar");
+    console.log("registerForm", this.registerForm.value);
+    console.log("Formulario válido:", this.registerForm.valid);
+
+    if (this.registerForm.valid) {
+      this.userService.register(this.registerForm.value).subscribe(
+        response => {
+          alert('Registro exitoso');
+        },
+        error => {
+          alert('Error al registrar usuario: ' + error.error);
+        }
+      );
+    } else {
+      alert('Formulario inválido');
+    }
+  }
 }
