f17392d749dcefb6634a1146ae56031a3158c661
Darwin Gomez <darwingomez@wposs.com>
2025-05-14 17:41:21 -0500

    probar crud

--- front_end/front_end_tickets_bus_pdn/src/app/services/auth-service.service.ts
+++ front_end/front_end_tickets_bus_pdn/src/app/services/auth-service.service.ts
@@ -44,4 +44,55 @@ export class AuthService {
   login(credentials: any) {
     return this.http.post(`${this.baseUrl}/login`, credentials);
   }
+<<<<<<< Updated upstream
+=======
+
+loginWithProvider(provider: string) {
+  console.log('Iniciando login con proveedor:', provider);
+
+  const auth = getAuth();
+  let authProvider;
+
+  switch (provider) {
+    case 'google':
+      authProvider = new GoogleAuthProvider();
+      break;
+    case 'facebook':
+      authProvider = new FacebookAuthProvider();
+      break;
+    case 'github':
+      authProvider = new GithubAuthProvider();
+      break;
+    default:
+      console.error('Proveedor no soportado:', provider);
+      throw new Error('Proveedor no soportado');
+  }
+
+  return signInWithPopup(auth, authProvider)
+    .then(result => {
+      console.log('Autenticación exitosa con Firebase:', result.user.email);
+      return this.http.get<any[]>(`http://localhost:8080/tests/listUsersProvider/${provider}`).toPromise();
+    })
+    .then(data => {
+      console.log('Respuesta del backend:', data);
+      return data;
+    })
+    .catch(error => {
+      console.error('Error en loginWithProvider:', error);
+      throw error;
+    });
+}
+
+  getFirebaseUsersByProvider(provider: string) {
+  return this.http.get<any[]>(`http://localhost:8080/tests/listUsersProvider/${provider}`);
+}
+  
+  getLocalUsers() {
+    return this.http.get<any[]>('http://localhost:8080/api/users/allUsers');
+  }
+  
+  deleteUser(document: string) {
+    return this.http.delete(`http://localhost:8080/api/users/${document}`);
+  }
+>>>>>>> Stashed changes
 }
